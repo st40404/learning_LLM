eceea4109bb94530eec65aed72fe6bb2c4201f22
y2370 <https://gitlab.usuntek.com>
2025-05-08 16:44:10 +0800

    update BERT lesson 7

--- scripts/test_bert.py
+++ scripts/test_bert.py
@@ -89,37 +89,130 @@ clear_output()
 #     tokens[masked_index] = t
 #     print("Top {} ({:2}%):{}".format(i, int(p.item() * 100), tokens[:10]), '...')
 
-#########################################
-## lesson 6 : tune reconize fake news  ##
-#########################################
+#########################################################
+## lesson 6 : tune reconize related or unrelated info  ##
+#########################################################
 ## get date from : https://www.kaggle.com/c/fake-news-pair-classification-challenge/data
 
+# import pandas as pd
+
+# # 簡單的數據清理，去除空白標題的 examples
+# df_train = pd.read_csv("./dataset/BERT/lesson6/fake-news-pair-classification-challenge/train.csv")
+# empty_title = ((df_train['title2_zh'].isnull()) \
+#                | (df_train['title1_zh'].isnull()) \
+#                | (df_train['title2_zh'] == '') \
+#                | (df_train['title2_zh'] == '0'))
+# df_train = df_train[~empty_title]
+
+# # 剔除過長的樣本以避免 BERT 無法將整個輸入序列放入記憶體不多的 GPU
+# MAX_LENGTH = 30
+# df_train = df_train[~(df_train.title1_zh.apply(lambda x : len(x)) > MAX_LENGTH)]
+# df_train = df_train[~(df_train.title2_zh.apply(lambda x : len(x)) > MAX_LENGTH)]
+
+# # 只用 1% 訓練數據看看 BERT 對少量標註數據有多少幫助
+# SAMPLE_FRAC = 0.01
+# df_train = df_train.sample(frac=SAMPLE_FRAC, random_state=9527)
+
+# # 去除不必要的欄位並重新命名兩標題的欄位名
+# df_train = df_train.reset_index()
+# df_train = df_train.loc[:, ['title1_zh', 'title2_zh', 'label']]
+# df_train.columns = ['text_a', 'text_b', 'label']
+
+# # idempotence, 將處理結果另存成 tsv 供 PyTorch 使用
+# df_train.to_csv("train.tsv", sep="\t", index=False)
+
+# print("訓練樣本數：", len(df_train))
+# df_train.head()
+
+
+#########################################################
+## lesson 7: transfer original article to BERT format  ##
+#########################################################
+from torch.utils.data import Dataset
 import pandas as pd
+import pysnooper
 
-# 簡單的數據清理，去除空白標題的 examples
-df_train = pd.read_csv("../dataset/BERT/lesson6/fake-news-pair-classification-challenge/train.csv")
-empty_title = ((df_train['title2_zh'].isnull()) \
-               | (df_train['title1_zh'].isnull()) \
-               | (df_train['title2_zh'] == '') \
-               | (df_train['title2_zh'] == '0'))
-df_train = df_train[~empty_title]
+class FakeNewsDataset(Dataset):
+    # 讀取前處理後的 tsv 檔並初始化一些參數
+    def __init__(self, mode, tokenizer):
+        assert mode in ["train", "test"]  # 一般訓練你會需要 dev set
+        self.mode = mode
+        # 大數據你會需要用 iterator=True
+        self.df = pd.read_csv(mode + ".tsv", sep="\t").fillna("")
+        self.len = len(self.df)
+        self.label_map = {'agreed': 0, 'disagreed': 1, 'unrelated': 2}
+        self.tokenizer = tokenizer  # 我們將使用 BERT tokenizer
+    
+    @pysnooper.snoop() # 加入以了解所有轉換過程
+    # 定義回傳一筆訓練 / 測試數據的函式
+    def __getitem__(self, idx):
+        if self.mode == "test":
+            text_a, text_b = self.df.iloc[idx, :2].values
+            label_tensor = None
+        else:
+            text_a, text_b, label = self.df.iloc[idx, :].values
+            # 將 label 文字也轉換成索引方便轉換成 tensor
+            label_id = self.label_map[label]
+            label_tensor = torch.tensor(label_id)
+            
+        # 建立第一個句子的 BERT tokens 並加入分隔符號 [SEP]
+        word_pieces = ["[CLS]"]
+        tokens_a = self.tokenizer.tokenize(text_a)
+        word_pieces += tokens_a + ["[SEP]"]
+        len_a = len(word_pieces)
+        
+        # 第二個句子的 BERT tokens
+        tokens_b = self.tokenizer.tokenize(text_b)
+        word_pieces += tokens_b + ["[SEP]"]
+        len_b = len(word_pieces) - len_a
+        
+        # 將整個 token 序列轉換成索引序列
+        ids = self.tokenizer.convert_tokens_to_ids(word_pieces)
+        tokens_tensor = torch.tensor(ids)
+        
+        # 將第一句包含 [SEP] 的 token 位置設為 0，其他為 1 表示第二句
+        segments_tensor = torch.tensor([0] * len_a + [1] * len_b, 
+                                        dtype=torch.long)
+        
+        return (tokens_tensor, segments_tensor, label_tensor)
+    
+    def __len__(self):
+        return self.len
+    
+    
+# 初始化一個專門讀取訓練樣本的 Dataset，使用中文 BERT 斷詞
+trainset = FakeNewsDataset("train", tokenizer=tokenizer)
 
-# 剔除過長的樣本以避免 BERT 無法將整個輸入序列放入記憶體不多的 GPU
-MAX_LENGTH = 30
-df_train = df_train[~(df_train.title1_zh.apply(lambda x : len(x)) > MAX_LENGTH)]
-df_train = df_train[~(df_train.title2_zh.apply(lambda x : len(x)) > MAX_LENGTH)]
+# 選擇第一個樣本
+sample_idx = 1
 
-# 只用 1% 訓練數據看看 BERT 對少量標註數據有多少幫助
-SAMPLE_FRAC = 0.01
-df_train = df_train.sample(frac=SAMPLE_FRAC, random_state=9527)
+# 將原始文本拿出做比較
+text_a, text_b, label = trainset.df.iloc[sample_idx].values
 
-# 去除不必要的欄位並重新命名兩標題的欄位名
-df_train = df_train.reset_index()
-df_train = df_train.loc[:, ['title1_zh', 'title2_zh', 'label']]
-df_train.columns = ['text_a', 'text_b', 'label']
+# 利用剛剛建立的 Dataset 取出轉換後的 id tensors
+tokens_tensor, segments_tensor, label_tensor = trainset[sample_idx]
 
-# idempotence, 將處理結果另存成 tsv 供 PyTorch 使用
-df_train.to_csv("train.tsv", sep="\t", index=False)
+# 將 tokens_tensor 還原成文本
+tokens = tokenizer.convert_ids_to_tokens(tokens_tensor.tolist())
+combined_text = "".join(tokens)
 
-print("訓練樣本數：", len(df_train))
-df_train.head()
+# 渲染前後差異，毫無反應就是個 print。可以直接看輸出結果
+print(f"""[原始文本]
+句子 1：{text_a}
+句子 2：{text_b}
+分類  ：{label}
+
+--------------------
+
+[Dataset 回傳的 tensors]
+tokens_tensor  ：{tokens_tensor}
+
+segments_tensor：{segments_tensor}
+
+label_tensor   ：{label_tensor}
+
+--------------------
+
+[還原 tokens_tensors]
+{combined_text}
+""")
