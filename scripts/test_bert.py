3c0527f1c383b4b8825e9a89768bf2db57065aeb
y2370 <https://gitlab.usuntek.com>
2025-05-08 16:59:53 +0800

    add BERT lesson 8

--- scripts/test_bert.py
+++ scripts/test_bert.py
@@ -128,9 +128,114 @@ clear_output()
 #########################################################
 ## lesson 7: transfer original article to BERT format  ##
 #########################################################
+# from torch.utils.data import Dataset
+# import pandas as pd
+# import pysnooper
+
+# class FakeNewsDataset(Dataset):
+#     # 讀取前處理後的 tsv 檔並初始化一些參數
+#     def __init__(self, mode, tokenizer):
+#         assert mode in ["train", "test"]  # 一般訓練你會需要 dev set
+#         self.mode = mode
+#         # 大數據你會需要用 iterator=True
+#         self.df = pd.read_csv(mode + ".tsv", sep="\t").fillna("")
+#         self.len = len(self.df)
+#         self.label_map = {'agreed': 0, 'disagreed': 1, 'unrelated': 2}
+#         self.tokenizer = tokenizer  # 我們將使用 BERT tokenizer
+
+#     @pysnooper.snoop() # 加入以了解所有轉換過程
+#     # 定義回傳一筆訓練 / 測試數據的函式
+#     def __getitem__(self, idx):
+#         if self.mode == "test":
+#             text_a, text_b = self.df.iloc[idx, :2].values
+#             label_tensor = None
+#         else:
+#             text_a, text_b, label = self.df.iloc[idx, :].values
+#             # 將 label 文字也轉換成索引方便轉換成 tensor
+#             label_id = self.label_map[label]
+#             label_tensor = torch.tensor(label_id)
+
+#         # 建立第一個句子的 BERT tokens 並加入分隔符號 [SEP]
+#         word_pieces = ["[CLS]"]
+#         tokens_a = self.tokenizer.tokenize(text_a)
+#         word_pieces += tokens_a + ["[SEP]"]
+#         len_a = len(word_pieces)
+
+#         # 第二個句子的 BERT tokens
+#         tokens_b = self.tokenizer.tokenize(text_b)
+#         word_pieces += tokens_b + ["[SEP]"]
+#         len_b = len(word_pieces) - len_a
+
+#         # 將整個 token 序列轉換成索引序列
+#         ids = self.tokenizer.convert_tokens_to_ids(word_pieces)
+#         tokens_tensor = torch.tensor(ids)
+        
+#         # 將第一句包含 [SEP] 的 token 位置設為 0，其他為 1 表示第二句
+#         segments_tensor = torch.tensor([0] * len_a + [1] * len_b, 
+#                                         dtype=torch.long)
+
+#         return (tokens_tensor, segments_tensor, label_tensor)
+    
+#     def __len__(self):
+#         return self.len
+
+
+# # 初始化一個專門讀取訓練樣本的 Dataset，使用中文 BERT 斷詞
+# trainset = FakeNewsDataset("train", tokenizer=tokenizer)
+
+# # 選擇第一個樣本
+# sample_idx = 1
+
+# # 將原始文本拿出做比較
+# text_a, text_b, label = trainset.df.iloc[sample_idx].values
+
+# # 利用剛剛建立的 Dataset 取出轉換後的 id tensors
+# tokens_tensor, segments_tensor, label_tensor = trainset[sample_idx]
+
+# # 將 tokens_tensor 還原成文本
+# tokens = tokenizer.convert_ids_to_tokens(tokens_tensor.tolist())
+# combined_text = "".join(tokens)
+
+# # 渲染前後差異，毫無反應就是個 print。可以直接看輸出結果
+# print(f"""[原始文本]
+# 句子 1：{text_a}
+# 句子 2：{text_b}
+# 分類  ：{label}
+
+# --------------------
+
+# [Dataset 回傳的 tensors]
+# tokens_tensor  ：{tokens_tensor}
+
+# segments_tensor：{segments_tensor}
+
+# label_tensor   ：{label_tensor}
+
+# --------------------
+
+# [還原 tokens_tensors]
+# {combined_text}
+# """)
+
+
+
+#########################################################
+## lesson 8:   ##
+#########################################################
+"""
+實作可以一次回傳一個 mini-batch 的 DataLoader
+這個 DataLoader 吃我們上面定義的 `FakeNewsDataset`，
+回傳訓練 BERT 時會需要的 4 個 tensors：
+- tokens_tensors  : (batch_size, max_seq_len_in_batch)
+- segments_tensors: (batch_size, max_seq_len_in_batch)
+- masks_tensors   : (batch_size, max_seq_len_in_batch)
+- label_ids       : (batch_size)
+"""
+
+from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset
 import pandas as pd
-import pysnooper
 
 class FakeNewsDataset(Dataset):
     # 讀取前處理後的 tsv 檔並初始化一些參數
@@ -142,8 +247,7 @@ class FakeNewsDataset(Dataset):
         self.len = len(self.df)
         self.label_map = {'agreed': 0, 'disagreed': 1, 'unrelated': 2}
         self.tokenizer = tokenizer  # 我們將使用 BERT tokenizer
-    
-    @pysnooper.snoop() # 加入以了解所有轉換過程
+
     # 定義回傳一筆訓練 / 測試數據的函式
     def __getitem__(self, idx):
         if self.mode == "test":
@@ -154,18 +258,18 @@ class FakeNewsDataset(Dataset):
             # 將 label 文字也轉換成索引方便轉換成 tensor
             label_id = self.label_map[label]
             label_tensor = torch.tensor(label_id)
-            
+
         # 建立第一個句子的 BERT tokens 並加入分隔符號 [SEP]
         word_pieces = ["[CLS]"]
         tokens_a = self.tokenizer.tokenize(text_a)
         word_pieces += tokens_a + ["[SEP]"]
         len_a = len(word_pieces)
-        
+
         # 第二個句子的 BERT tokens
         tokens_b = self.tokenizer.tokenize(text_b)
         word_pieces += tokens_b + ["[SEP]"]
         len_b = len(word_pieces) - len_a
-        
+
         # 將整個 token 序列轉換成索引序列
         ids = self.tokenizer.convert_tokens_to_ids(word_pieces)
         tokens_tensor = torch.tensor(ids)
@@ -173,46 +277,69 @@ class FakeNewsDataset(Dataset):
         # 將第一句包含 [SEP] 的 token 位置設為 0，其他為 1 表示第二句
         segments_tensor = torch.tensor([0] * len_a + [1] * len_b, 
                                         dtype=torch.long)
-        
+
         return (tokens_tensor, segments_tensor, label_tensor)
     
     def __len__(self):
         return self.len
-    
-    
+
+
 # 初始化一個專門讀取訓練樣本的 Dataset，使用中文 BERT 斷詞
 trainset = FakeNewsDataset("train", tokenizer=tokenizer)
 
-# 選擇第一個樣本
-sample_idx = 1
+# 這個函式的輸入 `samples` 是一個 list，裡頭的每個 element 都是
+# 剛剛定義的 `FakeNewsDataset` 回傳的一個樣本，每個樣本都包含 3 tensors：
+# - tokens_tensor
+# - segments_tensor
+# - label_tensor
+# 它會對前兩個 tensors 作 zero padding，並產生前面說明過的 masks_tensors
+def create_mini_batch(samples):
+    tokens_tensors = [s[0] for s in samples]
+    segments_tensors = [s[1] for s in samples]
+    
+    # 測試集有 labels
+    if samples[0][2] is not None:
+        label_ids = torch.stack([s[2] for s in samples])
+    else:
+        label_ids = None
+    
+    # zero pad 到同一序列長度
+    tokens_tensors = pad_sequence(tokens_tensors, 
+                                  batch_first=True)
+    segments_tensors = pad_sequence(segments_tensors, 
+                                    batch_first=True)
+    
+    # attention masks，將 tokens_tensors 裡頭不為 zero padding
+    # 的位置設為 1 讓 BERT 只關注這些位置的 tokens
+    masks_tensors = torch.zeros(tokens_tensors.shape, 
+                                dtype=torch.long)
+    masks_tensors = masks_tensors.masked_fill(
+        tokens_tensors != 0, 1)
+    
+    return tokens_tensors, segments_tensors, masks_tensors, label_ids
 
-# 將原始文本拿出做比較
-text_a, text_b, label = trainset.df.iloc[sample_idx].values
 
-# 利用剛剛建立的 Dataset 取出轉換後的 id tensors
-tokens_tensor, segments_tensor, label_tensor = trainset[sample_idx]
+# 初始化一個每次回傳 64 個訓練樣本的 DataLoader
+# 利用 `collate_fn` 將 list of samples 合併成一個 mini-batch 是關鍵
+BATCH_SIZE = 64
+trainloader = DataLoader(trainset, batch_size=BATCH_SIZE, 
+                         collate_fn=create_mini_batch)
 
-# 將 tokens_tensor 還原成文本
-tokens = tokenizer.convert_ids_to_tokens(tokens_tensor.tolist())
-combined_text = "".join(tokens)
+data = next(iter(trainloader))
 
-# 渲染前後差異，毫無反應就是個 print。可以直接看輸出結果
-print(f"""[原始文本]
-句子 1：{text_a}
-句子 2：{text_b}
-分類  ：{label}
+tokens_tensors, segments_tensors, \
+    masks_tensors, label_ids = data
 
---------------------
-
-[Dataset 回傳的 tensors]
-tokens_tensor  ：{tokens_tensor}
-
-segments_tensor：{segments_tensor}
-
-label_tensor   ：{label_tensor}
-
---------------------
-
-[還原 tokens_tensors]
-{combined_text}
+print(f"""
+tokens_tensors.shape   = {tokens_tensors.shape} 
+{tokens_tensors}
+------------------------
+segments_tensors.shape = {segments_tensors.shape}
+{segments_tensors}
+------------------------
+masks_tensors.shape    = {masks_tensors.shape}
+{masks_tensors}
+------------------------
+label_ids.shape        = {label_ids.shape}
+{label_ids}
 """)
